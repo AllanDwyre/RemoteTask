8674aea1bdfb24fc1069cad1374f581eae9589b6
AllanDwyre <allan.dwyre@gmail.com>
2024-07-30 02:57:47 +0200

    Feature : Editor : Add Optional type, And custom is appearance in the Unity Editor

---
+++ Assets/_project/Editor/OptionalPropertyDrawer.cs
@@ -0,0 +1,28 @@
+﻿using _project.scripts.Utils;
+using UnityEditor;
+using UnityEngine;
+
+namespace _project.Editor
+{
+    [CustomPropertyDrawer(typeof(Optional<>))]
+    public class OptionalPropertyDrawer: PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var valueProperty = property.FindPropertyRelative("value");
+            var enableProperty = property.FindPropertyRelative("enable");
+
+            position.width -= 24;
+            EditorGUI.BeginDisabledGroup(!enableProperty.boolValue);
+            EditorGUI.PropertyField(position, valueProperty, label, true);
+            EditorGUI.EndDisabledGroup();
+            
+            position.x += position.width + 24;
+            position.width = position.height = EditorGUI.GetPropertyHeight(enableProperty);
+            EditorGUI.PropertyField(position, valueProperty, label, true);
+            position.x -= position.width;
+            EditorGUI.PropertyField(position, enableProperty, GUIContent.none);
+
+        }
+    }
+}
---
+++ Assets/_project/scripts/Utils/Optional.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace _project.scripts.Utils
+{
+    [Serializable]
+    public class Optional<T>
+    {
+        [SerializeField] private T value;
+        [SerializeField] private bool enable;
+
+        public bool Enable => enable;
+        public T Value => value;
+
+        public Optional(T initialValue)
+        {
+            value = initialValue;
+            enable = true;
+        }
+
+    }
+}
