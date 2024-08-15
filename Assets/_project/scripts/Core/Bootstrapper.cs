e0d6a1e8e7a3c561024471f16596e9cd8dd2907e
AllanDwyre <allan.dwyre@gmail.com>
2024-08-15 16:01:00 +0200

    Feature : Bootstrapper (runtimes initializer)

---
+++ Assets/_project/scripts/Core/Bootstrapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using _project.scripts.Utils;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _project.scripts.Core
+{
+    public class Bootstrapper : MonoBehaviour
+    {
+        
+        private void Start()
+        {
+#if UNITY_EDITOR
+            Application.runInBackground = true;
+            
+            if (SceneManager.loadedSceneCount == 1)
+            {
+                SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
+            }
+
+            GetComponent<NetworkButtons>().enabled = SceneManager.GetSceneAt(1).name == "Gameplay";
+#endif
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Init()
+        {
+#if UNITY_EDITOR
+            var currentlyLoadedEditorScene = SceneManager.GetActiveScene();
+#endif
+            if (!SceneManager.GetSceneByName("Bootstrapper").isLoaded)
+            {
+                SceneManager.LoadScene("Bootstrapper");
+            }
+
+#if UNITY_EDITOR
+            if (currentlyLoadedEditorScene.IsValid())
+                SceneManager.LoadSceneAsync(currentlyLoadedEditorScene.name, LoadSceneMode.Additive);
+#else
+            SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Additive);
+#endif
+        }
+    }
+}
